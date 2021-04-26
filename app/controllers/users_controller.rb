14f11f2946ecacae70ad86af804ff8884026f47d
ibrahimmc <imoufarr@chicagobooth.edu>
2021-04-26 22:47:28 +0000

    Changes in users_controller to ensure users cannot access the Discover and Feed pages of other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_current_user_is_discoverer, only: %i[ discover ]
+  before_action :ensure_current_user_is_feed_owner, only: %i[ feed ]
 
   private
 
@@ -8,6 +10,18 @@ class UsersController < ApplicationController
         @user = User.find_by!(username: params.fetch(:username))
       else
         @user = current_user
+      end
+    end
+    
+    def ensure_current_user_is_discoverer
+      if current_user != @user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+      end
+    end
+
+    def ensure_current_user_is_feed_owner
+      if current_user != @user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
       end
     end
 end
